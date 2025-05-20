844d5d180f9923d17a18fb5c1566b76a8ea8220b
amo-hada <amirhdv83@gmail.com>
2025-05-20 15:12:51 +0330

    changing the Employee.cs to it's simple version

--- XSerializeSample/Employee.cs
+++ XSerializeSample/Employee.cs
@@ -1,54 +1,12 @@
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace XSerializeSample;
 
+[XmlSerializable]
 public class Employee
 {
-    public XElement EmployeeElement = XElement.Parse("<Employee></Employee>");
-
-    private int _id;
-
-    private string _lastName;
-
-    private string _firstName;
-    public int ID 
-    {
-        get => _id;
-        set
-        {
-            EmployeeElement.SetElementValue(nameof(ID), value);
-            _id = value;
-        }
-    }
-
-    public string FirstName
-    {
-        get => _firstName;
-        set
-        {
-            EmployeeElement.SetElementValue(nameof(FirstName), value);
-            _firstName = value;
-        }
-    }
-
-    public string LastName
-    {
-        get { return _lastName; }
-        set
-        {
-            EmployeeElement.SetElementValue(nameof(LastName), value);
-            _lastName = value;
-        }
-    }
-
-    public Employee()
-    {
-    }
-
-    public Employee(XElement input)
-    {
-        ID = Int32.Parse(input.Element("ID").Value);
-        FirstName = input.Element("FirstName").Value;
-        LastName = input.Element("LastName").Value;
-    }
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
