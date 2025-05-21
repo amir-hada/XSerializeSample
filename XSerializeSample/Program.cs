abd137bfd0d742afc3e05761babf7cb798ac09f3
amo-hada <amirhdv83@gmail.com>
2025-05-21 15:13:24 +0330

    Changing the Program.cs to a simple version

--- XSerializeSample/Program.cs
+++ XSerializeSample/Program.cs
@@ -1,12 +1,1 @@
-﻿using System.Xml.Linq;
-using XSerializeSample;
-
-var emp = new Employee
-{
-    ID = 15,
-    FirstName = "Ali",
-    LastName = "Bolhasani"
-};
-
-Console.WriteLine(emp._xElement);
-
+﻿Console.WriteLine("Hello World!");
